277888e56368c917e5c65c639eaf199eccfd3905
Alex Escobar <alex.escobar@tanium.com>
2026-01-09 16:05:15 +0100

    Printed an URL parameter with chi.URLParam

--- main.go
+++ main.go
@@ -17,6 +17,12 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:alexrabocse.me@gmail.com\">alexrabocse.me@gmail.com</a></p>")
 }
 
+func catchUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	user := chi.URLParam(r, "user")
+	fmt.Fprintf(w, "Hello, %s!", user)
+}
+
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
 
 // 	switch r.URL.Path {
@@ -29,23 +35,25 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 // 	}
 // }
 
-type Router struct{}
+// type Router struct{}
 
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not Found !!!!", http.StatusNotFound)
-	}
-}
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page not Found !!!!", http.StatusNotFound)
+// 	}
+// }
 
 func main() {
 
 	r := chi.NewRouter()
 	r.Get("/", homeHandler)
+	r.Get("/{user}", catchUserHandler)
+
 	r.Get("/contact", contactHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not Found !!!!", http.StatusNotFound)
