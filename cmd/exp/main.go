f819ba65a1ba8ad48ba00b0eeabf21836043b40a
Alex Escobar <alex.escobar@tanium.com>
2026-01-12 15:40:54 +0100

    First template example

---
+++ cmd/exp/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+// type User struct {
+// 	Name string
+// }
+
+func main() {
+
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	user := struct {
+		Name string
+	}{
+		Name: "Alex",
+	}
+
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+
+}
