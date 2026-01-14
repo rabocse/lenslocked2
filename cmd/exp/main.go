9cedbf28f10cab499956cd875b1c91f4632c809f
Alex Escobar <alex.escobar@tanium.com>
2026-01-14 17:52:00 +0100

    xss example and deleted old main.go by mistake :(

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,30 +1,27 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
-// type User struct {
-// 	Name string
-// }
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	bio := `<script>alert("You have been hacked!! XD") </script>`
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my site!!!! </h1> <p>Bio:"+bio+"</p>")
+}
 
 func main() {
 
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+	})
 
-	user := struct {
-		Name string
-	}{
-		Name: "Alex",
-	}
-
-	err = t.Execute(os.Stdout, user)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", r)
 
 }
