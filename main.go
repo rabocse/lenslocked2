3b51dc93f39a8f4a8167e32b3d2e3dc95f575773
Alex Escobar <alex.escobar@tanium.com>
2026-01-26 13:55:51 +0100

    Added executeTemplate function to render templates

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles("template/home.gohtml")
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
 		log.Printf("parsing template %v", err)
 		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
@@ -26,10 +27,21 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("template/home.gohtml")
+	executeTemplate(w, tplPath)
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("template/contact.gohtml")
+	executeTemplate(w, tplPath)
+}
+
 func main() {
 
 	r := chi.NewRouter()
 	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	})
